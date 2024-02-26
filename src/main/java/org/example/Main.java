f7843983c1dda0cb70f03bdaeeb7aa2b089dbff3
Alina Kalarash <bloha7895@gmail.com>
2024-02-26 06:01:44 +0200

    com it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        Connection connection = Database.getConnection();
+
+        Database database = Database.getInstance();
+
+
+//        database.executeUpdate("INSERT INTO test_table VALUES (7, 'Oleg')");
+        database.executeResult("SELECT * FROM mytable");
     }
 }
