2c1fdbd64f51f240f5a848b74e6fffd014832e62
Alina Kalarash <bloha7895@gmail.com>
2024-02-27 03:33:29 +0200

    connected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,29 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-//        Connection connection = Database.getConnection();
+////        Connection connection = Database.getConnection();
+//
+//        Database database = Database.getInstance();
+//
+//
+////        database.executeUpdate("INSERT INTO test_table VALUES (7, 'Oleg')");
+//        database.executeResult("SELECT * FROM mytable");
 
-        Database database = Database.getInstance();
+        String jdbcUrl = "jdbc:postgresql://localhost:32768/ladatabase";
+        String username = "postgres";
+        String password = "123";
+
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connected yupi");
 
 
-//        database.executeUpdate("INSERT INTO test_table VALUES (7, 'Oleg')");
-        database.executeResult("SELECT * FROM mytable");
+
+            connection.close();
     }
 }
